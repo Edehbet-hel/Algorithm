628b9a65e2aa57d892045ff698db638b7f7a6d08
Bethel <bethelededh668@gmail.com>
2024-08-11 15:09:25 -0700

    initial commit

---
+++ sum.js
@@ -0,0 +1,10 @@
+// let arr =(32,35,12,11,88,55)
+// for( let i=0;i< arr.length -1; i++){
+//     if(arr(i)>arr(i+1)){
+//         let a =arr(i)
+//         let b = arr(i+1)
+//         arr(i) = b
+//         arr(i+1) = a
+//     }
+
+// }
